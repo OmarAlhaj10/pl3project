e20810e7314293a93421263d4b9419feb10839f8
Omar Zakaria Mohy Al-dein <119157520+OmarAlhaj10@users.noreply.github.com>
2024-12-15 17:06:03 +0200

    Update Question.fs

--- Question.fs
+++ Question.fs
@@ -1,19 +1,1 @@
-﻿namespace QuizApp
 
-type QuestionType =
-    | MultipleChoice of string list
-    | WrittenAnswer
-
-type Question = {
-    QuestionText: string
-    QuestionType: QuestionType
-    CorrectAnswer: string
-}
-
-module QuizData =
-    let sampleQuiz = 
-        [
-            1, { QuestionText = "What is 2 + 2?"; QuestionType = WrittenAnswer; CorrectAnswer = "4" }
-            2, { QuestionText = "Which of these is a fruit?"; QuestionType = MultipleChoice ["Apple"; "Car"; "Rock"]; CorrectAnswer = "Apple" }
-            3, { QuestionText = "What is the capital of France?"; QuestionType = WrittenAnswer; CorrectAnswer = "Paris" }
-        ] |> Map.ofList
