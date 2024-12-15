d4932938f1165b5e4dd8c191a8ea40067e924669
Seifahmed63 <seifsword267@gmail.com>
2024-12-15 17:35:31 +0200

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -1,1 +1,17 @@
+open System
+open System.Windows.Forms
+open QuizApp
 
+let createQuizForm (quiz: Map<int, Question>) =
+    let form = new Form(Text = "Quiz Application", Width = 600, Height = 400)
+    
+    let questionLabel = new Label(Text = "", Top = 50, Width = 500)
+    let answerPanel = new Panel(Top = 100, Left = 100, Width = 400, Height = 100)
+    let nextButton = new Button(Text = "Next", Top = 250, Left = 200, Width = 100)
+    let finishButton = new Button(Text = "Finish", Top = 250, Left = 320, Width = 100)
+    finishButton.Enabled <- false
+
+    form.Controls.AddRange([| questionLabel; answerPanel; nextButton; finishButton |])
+
+    let mutable currentQuestion = 1
+    let mutable userAnswers = Map.empty<int, string>
