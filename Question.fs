bb881d83d7bb906034e021d42fafd80d87469845
Loaynabill <140346955+Loaynabill@users.noreply.github.com>
2024-12-15 17:31:57 +0200

    Update Question.fs

--- Question.fs
+++ Question.fs
@@ -9,3 +9,13 @@ type Question = {
     QuestionType: QuestionType
     CorrectAnswer: string
 }
+
+module QuizData =
+    let sampleQuiz = 
+        [
+            1, { QuestionText = "What is 2 + 2?"; QuestionType = WrittenAnswer; CorrectAnswer = "4" }
+            2, { QuestionText = "Which of these is a fruit?"; QuestionType = MultipleChoice ["apple"; "Car"; "Rock"]; CorrectAnswer = "apple" }
+            3, { QuestionText = "What is the capital of France?"; QuestionType = WrittenAnswer; CorrectAnswer = "paris" }
+            4, { QuestionText = "Which of these is a car?"; QuestionType = MultipleChoice ["bmw"; "audi"; "ferarri";"all the above"]; CorrectAnswer = "all the above" }
+            5, { QuestionText = "whats the name of this project?"; QuestionType = WrittenAnswer; CorrectAnswer = "pl3" }
+        ] |> Map.ofList
